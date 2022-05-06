588621822fb095d5f45363958675a0ee6b8724c6
Alex <Alex@zhp.com>
2022-05-06 16:24:56 +0800

    push commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("Git,hello hot-fix");
         System.out.println("Git,hello master test");
         System.out.println("Git,hello hot-fix test");
+        System.out.println("Git,push test");
     }
 
 }
