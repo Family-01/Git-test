5ddc6e8bebfc8a776b48a4e6b23775af1c2f9ea5
Alex <Alex@zhp.com>
2022-05-06 11:13:08 +0800

    idea third commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,6 +2,8 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("测试Idea集成Git...");
+        int a = 10;
+        System.out.println("Git version3!");
     }
 
 }
