961b9adfac166494fb6ad1ff7d95440945bc9904
Alex <Alex@zhp.com>
2022-05-06 11:31:45 +0800

    hot-fix first commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
         System.out.println("测试Idea集成Git...");
         int a = 10;
         System.out.println("Git version3!");
+        System.out.println("Git,hello hot-fix");
     }
 
 }
